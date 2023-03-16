10bcfc46b6336a115f1eec3a994f237bd77c2ef3
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-16 12:28:26 +0100

    Create src folder to improve project structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import me from "@/public/images/me.jpeg";
+import me from "/public/images/me.jpeg";
 
 export default function Home() {
   return (
