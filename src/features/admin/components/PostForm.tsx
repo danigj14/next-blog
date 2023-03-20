46dd40843cf1b9d397ccd4be2e192eb08026f135
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-20 21:09:28 +0100

    Refactor post creation Form to easily implement post update later.

---
+++ src/features/admin/components/PostForm.tsx
@@ -0,0 +1,80 @@
+import { Button, Input, TextArea } from "@/core/components";
+import { faBan, faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { marked } from "marked";
+import { FormEventHandler, useState } from "react";
+
+interface PostFormParams {
+  title: string;
+  description: string;
+  content: string;
+}
+
+interface PostFormProps {
+  initialValues?: PostFormParams;
+  onSubmit: (params: PostFormParams) => void;
+}
+
+export default function PostForm({
+  initialValues,
+  onSubmit = () => {},
+}: PostFormProps) {
+  const [title, setTitle] = useState(initialValues?.title || "");
+  const [description, setDescription] = useState(
+    initialValues?.description || ""
+  );
+  const [content, setContent] = useState(initialValues?.content || "");
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    onSubmit({title, description, content});
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col flex-grow">
+      <div className="flex pb-4 gap-4">
+        <h1 className="text-3xl">Create New Post</h1>
+        <Button
+          color="green"
+          className="ml-auto"
+          type="submit"
+        >
+          <FontAwesomeIcon icon={faCheck} className="pr-2" />
+          Save Post
+        </Button>
+        <Button color="red">
+          <FontAwesomeIcon icon={faBan} className="pr-2" />
+          Discard
+        </Button>
+      </div>
+      <div className="flex-grow flex flex-col gap-2">
+        <Input
+          placeholder="New Post Title"
+          onChange={(event) => setTitle(event.target.value)}
+          value={title}
+        />
+        <Input
+          placeholder="New Post Description"
+          onChange={(event) => setDescription(event.target.value)}
+          value={description}
+        />
+        <div className="flex-grow flex gap-4">
+          <TextArea
+            className="flex-1 resize-none overflow-auto"
+            placeholder="New Post Content (Markdown Syntax)"
+            onChange={(event) => setContent(event.target.value)}
+            value={content}
+          />
+          <div className="flex-1 min-h-full h-0 py-2 px-4 border border-gray-300 rounded-lg overflow-auto">
+            <div
+              className="prose"
+              dangerouslySetInnerHTML={{
+                __html: marked.parse(content || "New Post Content (Preview)"),
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
--- src/pages/admin/posts/create.tsx
+++ src/pages/admin/posts/create.tsx
@@ -1,5 +1,6 @@
 import { Button, Input, TextArea } from "@/core/components";
 import AdminLayout from "@/features/admin/components/AdminLayout";
+import PostForm from "@/features/admin/components/PostForm";
 import useCreatePostMutation from "@/features/posts/hooks/useCreatePostMutation";
 import { faBan, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,59 +8,11 @@ import { marked } from "marked";
 import { useState } from "react";
 
 export default function AdminCreatePost() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [content, setContent] = useState("");
-
   const createPostMutation = useCreatePostMutation();
 
   return (
     <AdminLayout>
-      <div className="flex pb-4 gap-4">
-        <h1 className="text-3xl">Create New Post</h1>
-        <Button
-          color="green"
-          className="ml-auto"
-          onClick={() =>
-            createPostMutation.mutate({ title, description, content })
-          }
-        >
-          <FontAwesomeIcon icon={faCheck} className="pr-4" />
-          Save Post
-        </Button>
-        <Button color="red">
-          <FontAwesomeIcon icon={faBan} className="pr-4" />
-          Discard
-        </Button>
-      </div>
-      <form className="flex-grow flex flex-col gap-2">
-        <Input
-          placeholder="New Post Title"
-          onChange={(event) => setTitle(event.target.value)}
-          value={title}
-        />
-        <Input
-          placeholder="New Post Description"
-          onChange={(event) => setDescription(event.target.value)}
-          value={description}
-        />
-        <div className="flex-grow flex gap-4">
-          <TextArea
-            className="flex-1 resize-none overflow-auto"
-            placeholder="New Post Content (Markdown Syntax)"
-            onChange={(event) => setContent(event.target.value)}
-            value={content}
-          />
-          <div className="flex-1 min-h-full h-0 py-2 px-4 border border-gray-300 rounded-lg overflow-auto">
-            <div
-              className="prose"
-              dangerouslySetInnerHTML={{
-                __html: marked.parse(content || "New Post Content (Preview)"),
-              }}
-            />
-          </div>
-        </div>
-      </form>
+      <PostForm onSubmit={(params) => createPostMutation.mutate(params)} />
     </AdminLayout>
   );
 }
