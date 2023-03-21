2501ec651f428cc022abf566c6041d41df9d6223
Dani <24252795+SGD14@users.noreply.github.com>
2023-03-21 10:32:42 +0100

    Implement success notification on post creation.

--- src/pages/admin/posts/create.tsx
+++ src/pages/admin/posts/create.tsx
@@ -1,3 +1,4 @@
+import useNotifications from "@/core/hooks/useNotifications";
 import AdminLayout from "@/features/admin/components/AdminLayout";
 import PostForm from "@/features/admin/components/PostForm";
 import useCreatePostMutation from "@/features/posts/hooks/useCreatePostMutation";
@@ -7,16 +8,26 @@ import { useRouter } from "next/router";
 export default function AdminCreatePost() {
   const createPostMutation = useCreatePostMutation();
   const { push } = useRouter();
+  const notifications = useNotifications();
+
+  const showSuccessNotification = () => {
+    notifications.showNotification({
+      message: "The post has been successfully created!",
+    });
+  };
 
   const onSubmit = (params: PostCreateParams) => {
     createPostMutation.mutate(params, {
-      onSuccess: () => push("/admin/posts"),
+      onSuccess: () => {
+        push("/admin/posts");
+        showSuccessNotification();
+      },
     });
   };
 
   return (
     <AdminLayout>
-      <PostForm onSubmit={onSubmit} />
+      <PostForm onSubmit={onSubmit} onDiscard={() => {}} />
     </AdminLayout>
   );
 }
